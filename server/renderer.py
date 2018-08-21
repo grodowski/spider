863465097fe813833be7aa651d05af83e70c3fdc
Jan Grodowski <jgrodowski@gmail.com>
2018-08-21 16:04:00 +0200

    use get

--- server/renderer.py
+++ server/renderer.py
@@ -12,10 +12,10 @@ class Renderer(object):
         return f"""
         <tr>
             <td>
-                <img src="{item['img_url']}" />
+                <img src="{item.get('img_url')}" />
             </td>
-            <td><a href="{item['url']}">{item['title']}</a></td>
-            <td>{item['price']}</td>
-            <td>{item['last_seen']}</td>
+            <td><a href="{item.get('url')}">{item.get('title')}</a></td>
+            <td>{item.get('price')}</td>
+            <td>{item.get('last_seen')}</td>
         </tr>
         """
